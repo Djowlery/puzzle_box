e51873b1a6d7ca17cc6bf48cbdff70bb485ddfbf
nadavschwalb <nadavschwalb@mail.tau.ac.il>
2020-06-09 14:49:00 +0100

    added timestamp to door

--- simple_puzzle_box.py
+++ simple_puzzle_box.py
@@ -3,7 +3,9 @@ from adafruit_motor import stepper
 import time
 import RPi.GPIO as IO
 from picamera import PiCamera
+from datetime import datetime
 
+#setup
 red = 25
 blue =24
 step_number =40
@@ -14,31 +16,35 @@ IO.setup(red,IO.IN,pull_up_down=IO.PUD_DOWN) #GPIO 23 -> IR sensor as input
 IO.setup(blue,IO.IN,pull_up_down=IO.PUD_DOWN) #GPIO 24 -> IR sensor as input
 
 
-
+#object instantiation
 kit = MotorKit()
 camera = PiCamera()
 kit.stepper1.release()
 
-
-while True:
-    if IO.input(red) == True:
-        print("red")
-        time.sleep(close_delay)
-        for i in range(step_number):
-            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-            time.sleep(0.08)
-        for i in range(round(step_number/2)):
-            kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-        kit.stepper1.release()
-    elif IO.input(blue) == True:
-        print("blue")
-        time.sleep(close_delay)
-        for i in range(step_number):
-            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-            time.sleep(0.08)
-        for i in range(round(step_number/2)):
-            kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-        kit.stepper1.release()
-    else:
-        print("unresolved")
-    time.sleep(0.1)
+#main loop
+with open("Puzzle_Data.csv", 'a') as puzzle_data:
+    
+    while True:
+        if IO.input(red) == True:
+            print("red")
+            puzzle_data.write("{},red".format(datetime.now()))
+            time.sleep(close_delay)
+            for i in range(step_number):
+                kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+                time.sleep(0.08)
+            for i in range(round(step_number/2)):
+                kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+            kit.stepper1.release()
+        elif IO.input(blue) == True:
+            puzzle_data.write("{},blue".format(datetime.now()))
+            print("blue")
+            time.sleep(close_delay)
+            for i in range(step_number):
+                kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+                time.sleep(0.08)
+            for i in range(round(step_number/2)):
+                kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+            kit.stepper1.release()
+        else:
+            print("unresolved")
+        time.sleep(0.1)
