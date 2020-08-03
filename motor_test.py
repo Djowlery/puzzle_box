c71c93afbfef2ba3a5b490fe429a3ae1d4366222
nadav <nadav@DESKTOP-LM1JD2A.localdomain>
2020-08-03 12:42:37 +0300

    updated motor test

--- motor_test.py
+++ motor_test.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
-
+print("test started")
 GpioPins = [18, 23 ,24, 25]
 motor = RpiMotorLib.BYJMotor("TestMotor", "28BYJ")
 motor.motor_run(GpioPins,0.001,512,False,False,"half",.001)
 
+print("test finished")
