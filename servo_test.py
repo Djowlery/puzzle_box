441dd8a188e7e5b9f1c70376ece35f0d8d6ecbff
nadav <nadav@DESKTOP-LM1JD2A.localdomain>
2020-08-09 13:06:51 +0300

    servo control

--- servo_test.py
+++ servo_test.py
@@ -2,15 +2,23 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import rpiservolib
 import time
 servoPin = 17
+servoStart = 1
+movePull = 6
+holdTime = 1
+servoDelay = 1
 feeder_servo = rpiservolib.SG90servo("feeder")
-feeder_servo.servo_move(servoPin,1)
-move = 0
-while move !=-1:
-    move = float(input())
-    feeder_servo.servo_move(servoPin,move)
-    print(move)
-    time.sleep(1)
-    feeder_servo.servo_move(servoPin,1)
+feeder_servo.servo_move(servoPin,servoStart)
+
+move = ''
+while True:
+    move = input()
+    if move == 'm':
+        feeder_servo.servo_move(servoPin,movePull,servoDelay)
+        time.sleep(holdTime)
+        feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+    else:
+        continue
+
   
    
 
