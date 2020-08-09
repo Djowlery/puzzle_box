673171f83ff76f4211a9583ae0b41f95adc48732
nadavschwalb <nadavschwalb@mail.tau.ac.il>
2020-08-09 12:01:53 +0100

    added full, half, open, close and quick

--- servo_test.py
+++ servo_test.py
@@ -3,21 +3,38 @@ from RpiMotorLib import rpiservolib
 import time
 servoPin = 17
 servoStart = 1
-movePull = 5
-holdTime = 1
-servoDelay = 1
+movePull = 7
+holdTime = 0.4
+servoDelay = 0.3
 feeder_servo = rpiservolib.SG90servo("feeder")
 feeder_servo.servo_move(servoPin,servoStart)
 
 move = ''
 while True:
     move = input()
-    if move == 'm':
+    if move == 'full':
         feeder_servo.servo_move(servoPin,movePull,servoDelay)
         time.sleep(holdTime)
         feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+    elif move == 'half':
+        feeder_servo.servo_move(servoPin,3.6,servoDelay)
+        time.sleep(holdTime)
+        feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+    elif move == 'quick':
+        feeder_servo.servo_move(servoPin,movePull,servoDelay)
+        feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+    elif move == 'open':
+        feeder_servo.servo_move(servoPin,movePull,servoDelay)
+        
+    elif move == "close":
+        feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+    elif move == 'quit':
+        feeder_servo.servo_move(servoPin,servoStart,servoDelay)
+        break
     else:
         continue
+print("servo test terminated")
+
 
   
    
